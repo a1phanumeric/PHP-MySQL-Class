f9121e0757c166fb04a977f17594b0be50945423
Ramanpreet Singh <50739802+raman3035@users.noreply.github.com>
2021-09-30 22:19:37 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,8 +2,9 @@ $dsn      = 'mysql:dbname=' . DATABASE_NAME . ';host=' . DATABASE_HOST.';charset
 $user     = DATABASE_USER;
 $password = DATABASE_PASS;
 
-You this line to make code simple :
+/*You this line to make code simple :
 
 include('connection.php');
 
 This will help in reading and understanding code easily
+*/
