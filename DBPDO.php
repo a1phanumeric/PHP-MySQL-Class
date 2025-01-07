7f8c3e3b7a7e115f0da3cb4304b0f23592280664
Ed Rackham <ed.rackham19@gmail.com>
2025-01-07 09:30:55 +0000

    Update DBPDO.php
    Updated DBPDO to use a a singleton instance if needed throughout the app

--- DBPDO.php
+++ DBPDO.php
@@ -5,84 +5,124 @@ namespace A1phanumeric;
 use \PDO;
 use \PDOException;
 
-class DBPDO {
+class DBPDO
+{
 
+	private static $instance = null;
 	public $pdo;
 	private $error;
 	private $dbname;
 	private $dbhost;
 	private $dbuser;
 	private $dbpass;
+	private $orderwise;
 
+	public static function getInstance($dbhost, $dbname, $dbuser, $dbpass, $orderwise = false)
+	{
+		if (self::$instance === null) {
+			self::$instance = new self($dbhost, $dbname, $dbuser, $dbpass, $orderwise);
+		}
+		return self::$instance;
+	}
 
-	function __construct($dbhost = '', $dbname = '', $dbuser = '', $dbpass= '') {
+	function __construct($dbhost = '', $dbname = '', $dbuser = '', $dbpass = '', $orderwise = false)
+	{
 		$this->dbhost = $dbhost;
 		$this->dbname = $dbname;
 		$this->dbuser = $dbuser;
 		$this->dbpass = $dbpass;
+		$this->orderwise = $orderwise;
 		$this->connect();
 	}
 
+	// Disallow cloning and unserializing
+    private function __clone() {}
+    private function __wakeup() {}
 
-	function prep_query($query){
+
+	function prep_query($query)
+	{
 		return $this->pdo->prepare($query);
 	}
 
 
-	function connect(){
-		if(!$this->pdo){
-
-			$dsn      = 'mysql:dbname=' . $this->dbname . ';host=' . $this->dbhost . ';charset=utf8';
+	function connect()
+	{
+		if (!$this->pdo) {
+			if($this->orderwise){
+				$dsn  = 'sqlsrv:Server=' . $this->dbhost . ';Database=' . $this->dbname . ';Encrypt=no';
+			}else{
+				$dsn  = 'mysql:dbname=' . $this->dbname . ';host=' . $this->dbhost . ';charset=utf8mb4';
+			}
 			$user     = $this->dbuser;
 			$password = $this->dbpass;
 
 			try {
-				$this->pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_PERSISTENT => true));
+				if($this->orderwise){
+					$this->pdo = new PDO($dsn, $user, $password);
+				}else{
+					$this->pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_PERSISTENT => true));
+				}
 				return true;
 			} catch (PDOException $e) {
 				$this->error = $e->getMessage();
-				die($this->error);
+				// die($this->error);
 				return false;
 			}
-		}else{
-			$this->pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+		} else {
+			$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 			return true;
 		}
 	}
 
 
-	function table_exists($table_name){
+	function table_exists($table_name)
+	{
 		$stmt = $this->prep_query('SHOW TABLES LIKE ?');
 		$stmt->execute(array($table_name));
 		return $stmt->rowCount() > 0;
 	}
 
 
-	function execute($query, $values = null){
-		if($values == null){
+	function execute($query, $values = null, $debug = false)
+	{
+		if ($values == null) {
 			$values = array();
-		}else if(!is_array($values)){
+		} else if (!is_array($values)) {
 			$values = array($values);
 		}
 		$stmt = $this->prep_query($query);
-		$stmt->execute($values);
+		if($debug){
+			echo $query;
+			print_r($values);
+			die();
+		}
+		try {
+			$stmt->execute($values);
+		} catch (PDOException $e) {
+			$this->error = $e->getMessage();
+			die($query . "<br />\n" . $this->error);
+			return false;
+		}
 		return $stmt;
 	}
 
-	function fetch($query, $values = null){
-		if($values == null){
+	function fetch($query, $values = null)
+	{
+		if ($values == null) {
 			$values = array();
-		}else if(!is_array($values)){
+		} else if (!is_array($values)) {
 			$values = array($values);
 		}
 		$stmt = $this->execute($query, $values);
 		return $stmt->fetch(PDO::FETCH_ASSOC);
 	}
 
-	function fetchAll($query, $values = null, $key = null){
-		if($values == null){
+	function fetchAll($query, $values = null, $key = null)
+	{
+		if ($values == null) {
 			$values = array();
-		}else if(!is_array($values)){
+		} else if (!is_array($values)) {
 			$values = array($values);
 		}
 		$stmt = $this->execute($query, $values);
@@ -91,7 +131,7 @@ class DBPDO {
 		// Allows the user to retrieve results using a
 		// column from the results as a key for the array
 		if(!empty($results)){
-			if ($key != null && $results[0][$key]) {
+			if ($key != null) {
 				$keyed_results = array();
 				foreach ($results as $result) {
 					$keyed_results[$result[$key]] = $result;
@@ -102,8 +142,8 @@ class DBPDO {
 		return $results;
 	}
 
-	function lastInsertId(){
+	function lastInsertId()
+	{
 		return $this->pdo->lastInsertId();
 	}
-
 }
